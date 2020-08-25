c5207adaf3e66d5c521c8ffad28f9f2aefc5f935
Jouke de Vries <joukev87@gmail.com>
2020-08-25 19:34:12 +0000

    Adjusted the styles of the messages and made them show up after other processes

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, HttpResponse
+from django.shortcuts import render, redirect, HttpResponse, get_object_or_404
 from django.contrib import messages
 
 from products.models import Product
@@ -14,7 +14,7 @@ def view_cart(request):
 def add_to_cart(request, item_id):
     """Add a a product to the cart"""
 
-    product = Product.objects.get(pk=item_id)
+    product = get_object_or_404(Product, pk=item_id)
     quantity = 1
     redirect_url = request.POST.get('redirect_url')
     cart = request.session.get('cart', {})
@@ -33,12 +33,13 @@ def add_to_cart(request, item_id):
 def remove_from_cart(request, item_id):
     """Remove the item from the cart"""
     try:
+        product = get_object_or_404(Product, pk=item_id)
         cart = request.session.get('cart', {})
         cart.pop(item_id)
         request.session['cart'] = cart
-
+        messages.warning(request, f'You removed {product.name} from your cart!')
         return HttpResponse(status=200)
 
     except Exception as e:
-        print('Got here')
+        messages.error(request, f"Removing {product.name} from your cart didn't work!")
         return HttpResponse(status=500)
