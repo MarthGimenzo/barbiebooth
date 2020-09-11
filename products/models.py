eebb51b870c8619560afa43e6ceb85062d76ffd4
Jouke de Vries <joukev87@gmail.com>
2020-09-11 17:13:39 +0000

    Edited form on checkout page and changed link styles

--- products/models.py
+++ products/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class Category(models.Model):
     class Meta:
         verbose_name_plural = 'Categories'
 
     name = models.CharField(max_length=254)
-    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+    friendly_name = models.CharField(max_length=254, blank=True)
     # created_date = models.DateTimeField(auto_now_add=True)
     # edited_date = models.DateTimeField(auto_now=True)
 
@@ -25,19 +26,20 @@ AVAILABILITY = (
 
 
 class Product(models.Model):
-    category = models.ForeignKey('Category', null=True, blank=True, 
+    category = models.ForeignKey('Category', null=True, blank=True,
                                  on_delete=models.SET_NULL)
-    sku = models.CharField(max_length=254, null=True, blank=True)
+    sku = models.CharField(max_length=254, blank=True)
     name = models.CharField(max_length=254)
     description = models.TextField()
     views = models.IntegerField(default=0)
-    year = models.CharField(max_length=5, null=True, blank=True)
+    year = models.CharField(max_length=5, blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     condition = models.DecimalField(max_digits=6, decimal_places=2,
                                     null=True, blank=True)
-    image_url = models.URLField(max_length=1024, null=True, blank=True)
-    image = models.ImageField(null=True, blank=True)
-    availabilty = models.CharField(max_length=50, default='available', choices=AVAILABILITY)
+    image_url = models.URLField(max_length=1024, blank=True)
+    image = models.ImageField(blank=True)
+    availabilty = models.CharField(max_length=50, default='available',
+                                   choices=AVAILABILITY)
 
     # created_date = models.DateTimeField(auto_now_add=True)
     # edited_date = models.DateTimeField(auto_now=True)
@@ -46,7 +48,8 @@ class Product(models.Model):
         return self.name
 
 # class ProductImage(models.Model):
-#     product = models.ForeignKey('Product', null=True, blank=True, on_delete=models.SET_NULL)
+#     product = models.ForeignKey('Product', null=True, blank=True,
+#                                 on_delete=models.SET_NULL)
 #     image_url = models.URLField(max_length=1024, null=True, blank=True)
 #     image = models.ImageField(null=True, blank=True)
 #     created_date = models.DateTimeField(auto_now_add=True)
