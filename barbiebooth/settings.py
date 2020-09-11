b50ce07755506fb45e75e03724fe504eb5f382f9
Jouke de Vries <joukev87@gmail.com>
2020-09-11 18:11:49 +0000

    Refactored some code

--- barbiebooth/settings.py
+++ barbiebooth/settings.py
@@ -53,8 +53,6 @@ INSTALLED_APPS = [
 
     'crispy_forms',
     'storages',
-
-
 ]
 
 MIDDLEWARE = [
@@ -220,4 +218,4 @@ else:
     EMAIL_HOST = 'smtp.gmail.com'
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
     EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
-    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
+    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER'),
