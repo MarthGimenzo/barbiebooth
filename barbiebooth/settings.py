0c86890a3f2ce20330caa0031fddf5f7bc28573c
Jouke de Vries <joukev87@gmail.com>
2020-09-12 11:02:12 +0000

    Changed email settings

--- barbiebooth/settings.py
+++ barbiebooth/settings.py
@@ -25,8 +25,8 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = 'DEVELOPMENT' in os.environ
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
+# DEBUG = True
 
 ALLOWED_HOSTS = ['barbiebooth.herokuapp.com', 'localhost']
 
@@ -211,7 +211,7 @@ STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET')
 
 if 'DEVELOPMENT' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-    DEFAULT_FROM_EMAIL = 'barbiebooth@example.com'
+    DEFAULT_FROM_EMAIL = 'joukev87@gmail.com'
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_USE_TLS = True
