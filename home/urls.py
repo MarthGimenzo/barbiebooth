34e1153281bc6d9bb0ed5a807da68362438c6804
Jouke de Vries <joukev87@gmail.com>
2020-09-14 16:43:09 +0000

    Added pages for contact, faqs, privacy_policy and terms_conditions

--- home/urls.py
+++ home/urls.py
@@ -4,4 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.index, name='home'),
     path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
+    path('faqs/', views.faqs, name='faqs'),
+    path('privacy_policy/', views.privacy_policy, name='privacy_policy'),
+    path('terms_conditions/', views.terms_conditions, name='terms_conditions'),
 ]
--- home/views.py
+++ home/views.py
@@ -13,3 +13,27 @@ def about(request):
     """A view to return the about page"""
 
     return render(request, 'home/about.html')
+
+
+def contact(request):
+    """A view to return the contact page"""
+
+    return render(request, 'home/contact.html')
+
+
+def faqs(request):
+    """A view to return the faqs page"""
+
+    return render(request, 'home/faqs.html')
+
+
+def privacy_policy(request):
+    """A view to return the privacy policy page"""
+
+    return render(request, 'home/privacy_policy.html')
+
+
+def terms_conditions(request):
+    """A view to return the terms&conditions page"""
+
+    return render(request, 'home/terms_conditions.html')
