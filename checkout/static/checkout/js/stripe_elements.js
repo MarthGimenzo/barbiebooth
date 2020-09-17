fe16091474c200ee8c0f9dfa79a177022dad5610
Jouke de Vries <joukev87@gmail.com>
2020-09-17 14:27:08 +0000

    Corrected all javascript code according to Javascript Beautifier

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -6,6 +6,7 @@
     https://stripe.com/docs/stripe-js
 */
 
+var Stripe;
 var stripePublicKey = $('#id_stripe_public_key').text().slice(1, -1);
 var clientSecret = $('#id_client_secret').text().slice(1, -1);
 var stripe = Stripe(stripePublicKey);
@@ -121,7 +122,7 @@ form.addEventListener('submit', function(ev) {
     }).fail(function() {	
         // just reload the page, the error will be in django messages	
         location.reload();	
-    })
+    });
 });
 
 
